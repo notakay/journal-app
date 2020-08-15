9d33a64e562ef1c52152ddce5eb3e67fe0fec03b
akay <aungkhantko@protonmail.com>
2020-08-15 10:42:01 +0630

    Added DraftJs

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import EditorPage from '../EditorPage/EditorPage'
+
+function App() {
+  return (
+    <div className="App">
+      <EditorPage />
+    </div>
+  );
+}
+
+export default App;
