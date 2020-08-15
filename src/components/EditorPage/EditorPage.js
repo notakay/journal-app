4c64a3dd8415a1a217dc85bf3238ce75eb595a94
akay <aungkhantko@protonmail.com>
2020-08-15 17:27:34 +0630

    Switched to functional component

--- src/components/EditorPage/EditorPage.js
+++ src/components/EditorPage/EditorPage.js
@@ -1,129 +1,106 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils, getDefaultKeyBinding, convertToRaw, convertFromRaw } from 'draft-js';
+import { Editor, EditorState, RichUtils, getDefaultKeyBinding, convertToRaw } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './EditorPage.css';
 import './RichEditor.css';
 
-// TODO
-// - Switch to functional component
-// - Fix string ref error
+function EditorPage() {
+    const [editorState, setEditorState] = React.useState(
+        EditorState.createEmpty()
+    );
 
-class EditorPage extends React.Component {
-    constructor(props) {
-        super(props);
+    const editor = React.useRef(null);
 
-        const content = window.localStorage.getItem('content');
-        if(content) {
-            this.state = { editorState: EditorState.createWithContent(
-                convertFromRaw(JSON.parse(content))
-            )};
-        } else {
-            this.state = { editorState: EditorState.createEmpty() }
-        }
-
-        this.focus = () => this.refs.editor.focus();
-        this.onChange = (editorState) => {
-            const contentState = editorState.getCurrentContent();
-            this.saveContent(contentState);
-            this.setState({ editorState })
-        };
-
-        this.handleKeyCommand = this._handleKeyCommand.bind(this);
-        this.mapKeyToEditorCommand = this._mapKeyToEditorCommand.bind(this);
-        this.toggleBlockType = this._toggleBlockType.bind(this);
-        this.toggleInlineStyle = this._toggleInlineStyle.bind(this);
+    function focusEditor() {
+        editor.current.focus();
     }
 
-    _handleKeyCommand(command, editorState) {
+    React.useEffect(() => {
+        focusEditor();
+    }, []);
+
+    function onChange(editorState) {
+        saveContent(editorState);
+        setEditorState(editorState);
+    }
+
+    function saveContent(editorState) {
+        const contentState = editorState.getCurrentContent();
+        window.localStorage.setItem('content', JSON.stringify(convertToRaw(contentState)));
+        console.log(window.localStorage.getItem('content'));
+    }
+
+    function handleKeyCommand(command, editorState) {
         const newState = RichUtils.handleKeyCommand(editorState, command);
         if (newState) {
-            this.onChange(newState);
+            onChange(newState);
             return true;
         }
         return false;
     }
 
-    _mapKeyToEditorCommand(e) {
+    function mapKeyToEditorCommand(e) {
         if (e.keyCode === 9 /* TAB */) {
             const newEditorState = RichUtils.onTab(
                 e,
-                this.state.editorState,
+                editorState,
                 4, /* maxDepth */
             );
-            if (newEditorState !== this.state.editorState) {
-                this.onChange(newEditorState);
+            if (newEditorState !== editorState) {
+                onChange(newEditorState);
             }
             return;
         }
         return getDefaultKeyBinding(e);
     }
 
-    _toggleBlockType(blockType) {
-        this.onChange(
+
+    function toggleBlockType(blockType) {
+        onChange(
             RichUtils.toggleBlockType(
-                this.state.editorState,
+                editorState,
                 blockType
             )
         );
     }
 
-    _toggleInlineStyle(inlineStyle) {
-        this.onChange(
+    function toggleInlineStyle(inlineStyle) {
+        onChange(
             RichUtils.toggleInlineStyle(
-                this.state.editorState,
+                editorState,
                 inlineStyle
             )
         );
     }
 
-    saveContent(content) {
-        console.log('saving content');
-        window.localStorage.setItem('content', JSON.stringify(convertToRaw(content)));
-    }
-
-    render() {
-        const { editorState } = this.state;
-
-        // If the user changes block type before entering any text, we can
-        // either style the placeholder or hide it. Let's just hide it now.
-        let className = 'RichEditor-editor';
-        var contentState = editorState.getCurrentContent();
-        if (!contentState.hasText()) {
-            if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-                className += ' RichEditor-hidePlaceholder';
-            }
-        }
-
-        return (
-            <div className="EditorPage">
-                <div className="RichEditor-root">
-                    <span>
-                        <BlockStyleControls
-                            editorState={editorState}
-                            onToggle={this.toggleBlockType}
-                        />
-                        <InlineStyleControls
-                            editorState={editorState}
-                            onToggle={this.toggleInlineStyle}
-                        />
-                    </span>
-                    <div className={className} onClick={this.focus}>
-                        <Editor
-                            blockStyleFn={getBlockStyle}
-                            customStyleMap={styleMap}
-                            editorState={editorState}
-                            handleKeyCommand={this.handleKeyCommand}
-                            keyBindingFn={this.mapKeyToEditorCommand}
-                            onChange={this.onChange}
-                            placeholder="What's up?"
-                            ref="editor"
-                            spellCheck={true}
-                        />
-                    </div>
+    return (
+        <div className="EditorPage">
+            <div className="RichEditor-root">
+                <BlockStyleControls
+                    editorState={editorState}
+                    onToggle={toggleBlockType}
+                />
+                <InlineStyleControls
+                    editorState={editorState}
+                    onToggle={toggleInlineStyle}
+                />
+                <div onClick={focusEditor}>
+                    <Editor
+                        blockStyleFn={getBlockStyle}
+                        customStyleMap={styleMap}
+                        ref={editor}
+                        editorState={editorState}
+                        handleKeyCommand={handleKeyCommand}
+                        keyBindingFn={mapKeyToEditorCommand}
+                        onChange={onChange}
+                        placeholder="What's up"
+                        spellCheck={true}
+                    />
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 // Custom overrides for "code" style.
